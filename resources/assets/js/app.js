5e41a7708ab81d8c85b0cf654c273a8c93129b21
ShawnRong <shawnr@ciandt.com>
2018-08-10 13:55:44 +0800

    Update index page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,6 +14,7 @@ import QForm from './components/QFormPreview.vue';
 import ViewDetail from './components/Detail/DetailBtnGroup.vue';
 import { VueSlideoutPanel } from 'vue2-slideout-panel';
 
+import CountUp from 'countup.js';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -23,6 +24,19 @@ import { VueSlideoutPanel } from 'vue2-slideout-panel';
 window.events = new Vue();
 window.flash = function(message, level='success') {
   window.events.$emit('flash', { message , level });
+};
+
+window.onload = function() {
+  const options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ',',
+    decimal: '.',
+  };
+  let number1 = new CountUp('number1', 0, 4766, 0, 4, options);
+  number1.start();
+  let number2 = new CountUp('number2', 0, 22331, 0, 4, options);
+  number2.start();
 };
 
 const app = new Vue({
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,31 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@section('content')
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="p-3 p-md-5 m-md-3 text-center">
+            <h1 class="display-4 font-weight-normal">人人可用的在线表单工具</h1>
+            <p class="lead font-weight-normal">
+                帮你收集和管理日常工作中的数据，把几小时的工作缩减至零。
+            </p>
+            <a href="{{ route('login') }}" class="btn btn-outline-secondary">开始使用</a>
+        </div>
+        <div class="product-device shadow-sm d-none d-md-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+    <div class="d-md-flex flex-equal w-100 pl-md-3 my-md-3">
+        <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 p-3">
+                <p class="lead text-left">已经有</p>
+                <h2 class="display-3" id="number1">1234</h2>
+                <p class="lead text-right">份表单生成</p>
             </div>
         </div>
-    </body>
-</html>
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <p class="lead text-left">已经有</p>
+                <h2 class="display-3" id="number2">21234</h2>
+                <p class="lead text-right">人完成表单填写</p>
+            </div>
+        </div>
+    </div>
+@endsection
